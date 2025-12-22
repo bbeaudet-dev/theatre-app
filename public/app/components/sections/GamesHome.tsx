c65bd02bbad5b3aa0796f9804ba7f1473ca4423d
Ben Beaudet <bbeaudet0@gmail.com>
2025-12-22 10:58:17 -0500

    Removed personal-portfolio app reference

--- public/app/components/sections/GamesHome.tsx
+++
@@ -1,6 +0,0 @@
-import { gamesData } from 'app/for-fun/games/games-data'
-import { type GameData } from 'app/for-fun/games/utils'
-
-export function GamesHome(): GameData[] {
-  return gamesData
-} 
--- public/app/components/sections/index.ts
+++
@@ -1,9 +0,0 @@
-export { BlogHome } from './BlogHome'
-export { GamesHome } from './GamesHome'
-export { PortfolioHome } from './PortfolioHome'
-export { TheatreHome } from './TheatreHome'
-
-export type { BlogItem } from './BlogHome'
-export type { PortfolioItem } from './PortfolioHome'
-export type { TheatreItem } from './TheatreHome'
-export type { GameData } from 'app/for-fun/games/utils' 
